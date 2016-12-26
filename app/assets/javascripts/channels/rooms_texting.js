c000235d04ea3e767822f64536d06843ed195055
Adam Pahlevi <adam.pahlevi@gmail.com>
2016-12-26 13:51:36 +0700

    `rails g channel RoomsTexting`

---
+++ app/assets/javascripts/channels/rooms_texting.js
@@ -0,0 +1,13 @@
+App.roomsTexting = App.cable.subscriptions.create("RoomsTextingChannel", {
+    connected: function() {
+        // Called when the subscription is ready for use on the server
+    },
+
+    disconnected: function() {
+        // Called when the subscription has been terminated by the server
+    },
+
+    received: function(data) {
+        // Called when there's incoming data on the websocket for this channel
+    }
+});
