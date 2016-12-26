b79791b7babb0d4c0acc5dd60741232c9cc3aab5
Adam Pahlevi <adam.pahlevi@gmail.com>
2016-12-26 15:52:22 +0700

    implement `items` to iterate over items data on chat

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -13,6 +13,34 @@ class Chat < ApplicationRecord
   end
 
   def items
-    []
+    items = []
+    return items unless itemable?
+
+    # hapus semua text sekitar jual sampai spasi terakhir
+    clean_text = text.dup.gsub(ITEMABLE_REGEX, '')
+    
+    # karena satu chat bisa berisi lebih dari 1 item yang dipisahkan
+    # oleh titik koma
+    items_text = clean_text.split(';')
+
+    # iterasi untuk setiap item yang terkandung dalam
+    # text yang belum di parsing
+    items_text.each do |item_text|
+      # pisahkan item text berdasarkan tanda koma,
+      # data pertama berisi nama item, data kedua berisi harga,
+      # kemudian ambil kedua data tersebut untuk setiap iterasinya
+      item_text.split(',').each_slice(2) do |name, price|
+        # tambahkan ke koleksi items
+        items << {
+          name: name,
+          price: price.tr('.', '').to_i
+        }
+      end
+    end
+
+    # item yang sudah di parse dari text chat, setiap item berisi
+    # data nama dan harga yang terkandung dalam struktur data
+    # berupa hash/dictionary/associative array
+    items
   end
 end
