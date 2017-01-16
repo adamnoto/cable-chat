9ea3a15a23dc63b57c4c8f570dbee1a8b17faaf7
Adam Pahlevi <adam.pahlevi@gmail.com>
2017-01-16 18:13:02 +0700

    `rails g veritrans:install` with slight modif

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  ENV['MIDTRANS_CLIENT_KEY'] = 'VT-client-IKktHiy3aRYHljsw'.freeze
+  ENV['MIDTRANS_SERVER_KEY'] = 'VT-server-7CVlR3AJ8Dpkez3k_TeGJQZU'.freeze
 end
